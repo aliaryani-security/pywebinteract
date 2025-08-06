558f233efdf74e1eb9666690c370a2fb1d4a12a1
Ali Aryani <lenesis@proton.me>
2025-08-06 17:21:40 +0330

    commented ending

--- parse.py
+++ parse.py
@@ -12,3 +12,6 @@ page = url.read()
 soup = BeautifulSoup (page, features="html.parser")
 
 print (soup)
+# This project is intentionally left here!
+# I will write a whole new project about beautiful soup later,
+# This isn't the right place for it, is it ?!
